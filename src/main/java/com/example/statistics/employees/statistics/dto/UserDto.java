9bd38120b08e8446f72688e3810b25348dc82913
mayssal@epactunis.com <mayssal@epactunis.com>
2024-10-31 14:48:59 +0100

    add unit tests + integration tests

--- src/main/java/com/example/statistics/employees/statistics/dto/UserDto.java
+++ src/main/java/com/example/statistics/employees/statistics/dto/UserDto.java
@@ -1,9 +1,14 @@
 package com.example.statistics.employees.statistics.dto;
 
+import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
 import lombok.Getter;
 import lombok.Setter;
+import lombok.ToString;
+
 @Getter
 @Setter
+@ToString
+@JsonIgnoreProperties(ignoreUnknown = true)
 public class UserDto {
     private Long userId;
     private String firstName;
